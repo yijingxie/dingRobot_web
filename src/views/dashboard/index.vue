c565f93acda16de5bf208a7241f5ba3c93631d0d
Xyj <1779308193@qq.com>
2024-09-12 12:11:30 +0800

    初始代码

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    <h1>Vue3-Element-Admin-Thin</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "Dashboard",
+  inheritAttrs: false,
+});
+</script>
+
+<style lang="scss" scoped></style>
